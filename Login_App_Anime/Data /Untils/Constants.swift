ae5c259300923163bdcbd62d868a9046e6ffc537
wiski <wiski2311@gmail.com>
2026-02-09 21:03:08 +0100

    TableView Anime aaaaaa

---
+++ Login_App_Anime/Data /Untils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  FreeToGame
+//
+//  Created by Tardes on 20/1/26.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let SERVER_BASE_URL: String = "https://api.jikan.moe/v4/anime"
+}
---
+++ Login_App_Anime/ViewCells/AnimeCellView.swift
@@ -0,0 +1,8 @@
+//
+//  AnimeCellView.swift
+//  Login_App_Anime
+//
+//  Created by Tardes on 9/2/26.
+//
+
+import Foundation
